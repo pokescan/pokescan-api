f0b38322b0c5bb056dd6aa640e9ac3ea4806aaea
canserkanuren <canserkan.uren@gmail.com>
2021-06-19 20:17:55 +0200

    feat: add language service to store current request language

--- src/shared/middlewares/language.middleware.ts
+++ src/shared/middlewares/language.middleware.ts
@@ -1,13 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { LANGUAGE_HEADER_KEY } from '@shared/constants';
+import { LanguageService } from '@shared/services/language/language.service';
 import { Request, Response } from 'express';
 
 @Injectable()
 export class LanguageMiddleware implements NestMiddleware {
+  constructor(private readonly languageService: LanguageService) {}
+
   use(req: Request, res: Response, next: () => void): void {
-    console.log(
-      '🚀 ~ file: language.middleware.ts ~ line 7 ~ LanguageMiddleware ~ use ~ req',
-      req.headers
-    );
+    const languageInHeader: string = req.headers[LANGUAGE_HEADER_KEY] as string;
+
+    this.languageService.currentLanguage = languageInHeader;
+
     next();
   }
 }
