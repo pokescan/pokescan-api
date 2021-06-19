ceee5a915ae908e86b0d8ecba615a2cfbfaa5cf9
canserkanuren <canserkan.uren@gmail.com>
2021-06-19 20:23:25 +0200

    feat: remove ids from explicit args in mutations

--- src/pokemon-move-detail/dto/update-pokemon-move-detail.dto.ts
+++ src/pokemon-move-detail/dto/update-pokemon-move-detail.dto.ts
@@ -1,7 +1,13 @@
-import { InputType, PartialType } from '@nestjs/graphql';
+import { Field, InputType, PartialType } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { CreatePokemonMoveDetailDto } from './create-pokemon-move-detail.dto';
 
 @InputType()
 export class UpdatePokemonMoveDetailDto extends PartialType(
   CreatePokemonMoveDetailDto
-) {}
+) {
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+}
