64a9aac604925c3d0fa6703ff73bf604512a85c6
canserkanuren <canserkan.uren@gmail.com>
2021-06-26 13:23:08 +0200

    fix(pokemon-encounter): add pokemon variable in interface && in output dto

--- src/pokemon-encounter/interface/pokemon-encounter.interface.ts
+++ src/pokemon-encounter/interface/pokemon-encounter.interface.ts
@@ -1,12 +1,13 @@
 import { IGameVersion } from '@game-version/interface/game-version.interface';
 import { ILocation } from '@location/interface/location.interface';
+import { IPokemon } from '@pokemon/interface/pokemon.interface';
 import { TranslatableObject } from '@shared/models/translatable';
 
 export interface IPokemonEncounter {
   name: TranslatableObject[];
   gameVersion: IGameVersion;
   location: ILocation;
-  // pokemon: IPokemon;
+  pokemon: IPokemon;
   method: string;
   chance: number;
   minLevel: number;
