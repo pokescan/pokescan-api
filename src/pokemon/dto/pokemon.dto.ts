dc5af5b511423ba3284422a54e4b382b0bbe4ff1
Julien Mifsud <julien.mifsud77@gmail.com>
2021-06-27 15:54:42 +0200

    feat(pokemon): add pokemon constructor

--- src/pokemon/dto/pokemon.dto.ts
+++ src/pokemon/dto/pokemon.dto.ts
@@ -4,6 +4,7 @@ import { GenerationDto } from '@generation/dto/generation.dto';
 import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
 import { PokemonMoveDto } from '@pokemon-move/dto/pokemon-move.dto';
 import { PokemonTypeDto } from '@pokemon-type/dto/pokemon-type.dto';
+import { IPokemon } from '@pokemon/interface/pokemon.interface';
 import { PokemonGenderRepartitionObject } from '@pokemon/models/pokemon-gender-repartition';
 import { PokemonStatObject } from '@pokemon/models/pokemon-stat';
 import { CommonDto } from '@shared/models/common.dto';
@@ -73,4 +74,28 @@ export class PokemonDto extends CommonDto {
 
   @Field(() => PokemonDto, { nullable: true })
   nextPokemon: PokemonDto;
+
+  constructor(model?: IPokemon) {
+    super(model);
+
+    if (model) {
+      this.name = model.name;
+      this.abilities = model.abilities;
+      this.captureRate = model.captureRate;
+      this.cycle = model.cycle;
+      this.description = model.description;
+      this.eggsGroup = model.eggsGroup;
+      this.firstAppearenceGeneration = model.firstAppearenceGeneration;
+      this.genderRepartition = model.genderRepartition;
+      this.generation = model.generation;
+      this.height = model.height;
+      this.weight = model.weight;
+      this.nextPokemon = model.nextPokemon;
+      this.previousPokemon = model.previousPokemon;
+      this.pokedexId = model.pokedexId;
+      this.pokemonMoves = model.pokemonMoves;
+      this.pokemonStats = model.pokemonStats;
+      this.step = model.step;
+    }
+  }
 }
